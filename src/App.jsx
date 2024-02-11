dffd0454fbb04d4a53c241ce22c1772b49163cb8
37Evgeny <pysskuu88eicloud.com>
2024-02-11 16:20:22 +0300

    switch

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,22 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  const [open, setOpen] = useState(false)
 
-  console.log(open)
+  const [theme, setTheme] = useState('light');
 
-  const nextCount = () => {
-    setCount(count + 1)
-  }
+  const handleToogle = () => {
+    setTheme((toggle) => (toggle === 'light' ? 'dark' : 'light'))
+  };
 
-  const prevCount = () => {
-    setCount(count - 1)
-  }
 
   return (
-    <div className="App">
-
-        {/* <p className='name'>
-            Счетчик
-        </p>
-        <h1 className='counter'>
-            {count}
-        </h1>
-        <div className='btn__cnt'>
-        <button onClick={prevCount} className='prev'>
-            минус
-        </button>
-        <button onClick={nextCount} className='next'>
-            плюс
-        </button>
-        </div> */}
-       
-        <div className='modal__cnt'>
-            <div className='modal__open__btn'>
-              <button onClick={()=>setOpen(true)} className='btn'>
-                  Открыть окно
-              </button>
-            { open &&
-                <div className='modal'>
-                  <img src='./img/cat.jpeg' className='img__modal'/>
-                  <img onClick={()=>setOpen(false)} src='./img/close.svg' className='close'/>
-                 
-              </div>
-            }
-            </div>
-        </div>
+    <div className="App" id={theme}>
+      <span>{theme}</span>
+     <label className="switch">
+        <input onChange={handleToogle}  type="checkbox" className="slider"/>
+        <span  className='slider' ></span>
+     </label>
     </div>
-
-    
   );
 }
 
