fee5ec18c8aa179dadb292a65db2f87ab98463ae
37Evgeny <pysskuu88eicloud.com>
2024-02-13 21:51:12 +0300

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import { Slider } from '../src/components/Slider/Slider'
+import photo_1 from '../src/img/1.jpeg'
+import photo_2 from '../src/img/2.jpeg'
+import photo_3 from '../src/img/3.jpeg'
+import photo_4 from '../src/img/4.jpeg'
+import photo_5 from'../src/img/5.jpeg'
+import photo_6 from '../src/img/6.png'
 
 function App() {
-
-
-  const [theme, setTheme] = useState('light');
-
-  const handleToogle = () => {
-    setTheme((toggle) => (toggle === 'light' ? 'dark' : 'light'))
-  };
+  const sliderData = [
+    {
+      img: photo_1
+    },
+    {
+      img: photo_2
+    },
+    {
+      img: photo_3
+    },
+    {
+      img: photo_4
+    },
+    {
+      img: photo_5
+    },
+    {
+      img: photo_6
+    },
+  ]
 
 
   return (
-    <div className="App" id={theme}>
-      <span>{theme}</span>
-     <label className="switch">
-        <input onChange={handleToogle}  type="checkbox" className="slider"/>
-        <span  className='slider' ></span>
-     </label>
+    <div className="App">
+      <div className="content">
+         <Slider data={sliderData} />
+      </div>
     </div>
   );
 }
